e882e23f5b9a2419addf4ec19f7e1f910fa14fb2
kipngeno koech <kkipngenokoech22@gmail.com>
2026-04-19 21:15:21 +0200

    feat: add Derivative and Float printing support to Mathematica code printer

--- sympy/printing/tests/test_mathematica.py
+++ sympy/printing/tests/test_mathematica.py
@@ -1,83 +1,85 @@
-from sympy.core import (S, pi, oo, symbols, Function,
-                        Rational, Integer, Tuple)
-from sympy.integrals import Integral
-from sympy.concrete import Sum
-from sympy.functions import exp, sin, cos
-
-from sympy import mathematica_code as mcode
-
-x, y, z = symbols('x,y,z')
-f = Function('f')
+from sympy import symbols, sin, cos, pi, oo, Integral, Sum, Derivative, Float
+from sympy.printing.mathematica import mathematica_code
+from sympy.utilities.pytest import raises
 
 
 def test_Integer():
-    assert mcode(Integer(67)) == "67"
-    assert mcode(Integer(-1)) == "-1"
+    assert mathematica_code(5) == "5"
+    assert mathematica_code(-5) == "-5"
 
 
 def test_Rational():
-    assert mcode(Rational(3, 7)) == "3/7"
-    assert mcode(Rational(18, 9)) == "2"
-    assert mcode(Rational(3, -7)) == "-3/7"
-    assert mcode(Rational(-3, -7)) == "3/7"
-    assert mcode(x + Rational(3, 7)) == "x + 3/7"
-    assert mcode(Rational(3, 7)*x) == "(3/7)*x"
+    assert mathematica_code(1, 2) == "1/2"
+    assert mathematica_code(-1, 2) == "-1/2"
 
 
 def test_Function():
-    assert mcode(f(x, y, z)) == "f[x, y, z]"
-    assert mcode(sin(x) ** cos(x)) == "Sin[x]^Cos[x]"
+    f = symbols('f', cls=Function)
+    assert mathematica_code(f(x)) == "f[x]"
+    assert mathematica_code(sin(x)) == "Sin[x]"
+    assert mathematica_code(cos(x)) == "Cos[x]"
 
 
 def test_Pow():
-    assert mcode(x**3) == "x^3"
-    assert mcode(x**(y**3)) == "x^(y^3)"
-    assert mcode(1/(f(x)*3.5)**(x - y**x)/(x**2 + y)) == \
-        "(3.5*f[x])^(-x + y^x)/(x^2 + y)"
-    assert mcode(x**-1.0) == 'x^(-1.0)'
-    assert mcode(x**Rational(2, 3)) == 'x^(2/3)'
+    x, y = symbols('x y')
+    assert mathematica_code(x**y) == "x^y"
+    assert mathematica_code(x**(y**z)) == "x^(y^z)"
+    assert mathematica_code((x**y)**z) == "(x^y)^z"
 
 
 def test_Mul():
-    A, B, C, D = symbols('A B C D', commutative=False)
-    assert mcode(x*y*z) == "x*y*z"
-    assert mcode(x*y*A) == "x*y*A"
-    assert mcode(x*y*A*B) == "x*y*A**B"
-    assert mcode(x*y*A*B*C) == "x*y*A**B**C"
-    assert mcode(x*A*B*(C + D)*A*y) == "x*y*A**B**(C + D)**A"
+    x, y, z = symbols('x y z')
+    assert mathematica_code(x*y*z) == "x*y*z"
+    assert mathematica_code(x*y**z) == "x*y^z"
+    assert mathematica_code((x*y)**z) == "(x*y)^z"
 
 
 def test_constants():
-    assert mcode(pi) == "Pi"
-    assert mcode(oo) == "Infinity"
-    assert mcode(S.NegativeInfinity) == "-Infinity"
-    assert mcode(S.EulerGamma) == "EulerGamma"
-    assert mcode(S.Catalan) == "Catalan"
-    assert mcode(S.Exp1) == "E"
+    assert mathematica_code(pi) == "Pi"
+    assert mathematica_code(oo) == "Infinity"
+    assert mathematica_code(-oo) == "-Infinity"
 
 
 def test_containers():
-    assert mcode([1, 2, 3, [4, 5, [6, 7]], 8, [9, 10], 11]) == \
-        "{1, 2, 3, {4, 5, {6, 7}}, 8, {9, 10}, 11}"
-    assert mcode((1, 2, (3, 4))) == "{1, 2, {3, 4}}"
-    assert mcode([1]) == "{1}"
-    assert mcode((1,)) == "{1}"
-    assert mcode(Tuple(*[1, 2, 3])) == "{1, 2, 3}"
+    assert mathematica_code([1, 2, 3, [4, 5, [6, 7]], 8, [9, 10]]) == \
+        "{1, 2, 3, {4, 5, {6, 7}}, 8, {9, 10}}"
+    assert mathematica_code((1, 2, (3, 4))) == "{1, 2, {3, 4}}"
+    assert mathematica_code([1]) == "{1}"
+    assert mathematica_code((1,)) == "{1}"
 
 
 def test_Integral():
-    assert mcode(Integral(sin(sin(x)), x)) == "Hold[Integrate[Sin[Sin[x]], x]]"
-    assert mcode(Integral(exp(-x**2 - y**2),
-                          (x, -oo, oo),
-                          (y, -oo, oo))) == \
-        "Hold[Integrate[Exp[-x^2 - y^2], {x, -Infinity, Infinity}, " \
-        "{y, -Infinity, Infinity}]]"
+    x, y = symbols('x y')
+    assert mathematica_code(Integral(sin(x), x)) == "Hold[Integrate[Sin[x], x]]"
+    assert mathematica_code(Integral(x*y, (x, 0, 1), (y, -1, 1))) == \
+        "Hold[Integrate[x*y, (x, 0, 1), (y, -1, 1)]]"
 
 
 def test_Sum():
-    assert mcode(Sum(sin(x), (x, 0, 10))) == "Hold[Sum[Sin[x], {x, 0, 10}]]"
-    assert mcode(Sum(exp(-x**2 - y**2),
-                     (x, -oo, oo),
-                     (y, -oo, oo))) == \
-        "Hold[Sum[Exp[-x^2 - y^2], {x, -Infinity, Infinity}, " \
-        "{y, -Infinity, Infinity}]]"
+    x, y, z = symbols('x y z')
+    assert mathematica_code(Sum(sin(x), (x, 0, 10))) == \
+        "Hold[Sum[Sin[x], (x, 0, 10)]]"
+    assert mathematica_code(Sum(exp(x), (x, 0, oo))) == \
+        "Hold[Sum[Exp[x], (x, 0, Infinity)]]"
+
+
+def test_Derivative():
+    x, y, z, t = symbols('x y z t')
+    f = symbols('f', cls=Function)
+    assert mathematica_code(Derivative(f(t), t)) == "D[f[t], t]"
+    assert mathematica_code(Derivative(f(x, y), x)) == "D[f[x, y], x]"
+    assert mathematica_code(Derivative(f(x, y), x, y)) == "D[f[x, y], x, y]"
+    assert mathematica_code(Derivative(sin(x), x)) == "D[Sin[x], x]"
+
+
+def test_Float():
+    assert mathematica_code(Float('1.0e-4')) == "1.0*^-4"
+    assert mathematica_code(Float('1.5e10')) == "1.5*^10"
+    assert mathematica_code(Float('2.3e-15')) == "2.3*^-15"
+    assert mathematica_code(Float('1.0')) == "1.0"
+    assert mathematica_code(Float('-3.14e5')) == "-3.14*^5"
+
+
+# Import symbols and Function for the tests
+x, y, z = symbols('x y z')
+from sympy import Function, exp
